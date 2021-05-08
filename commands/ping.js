56a4aba3f5523d2b19ce1102b40a2a98a5f3e701
Nolan Lopes <55511025+MRnolan1030@users.noreply.github.com>
2021-05-08 18:45:32 +0200

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -8,11 +8,12 @@ module.exports.run = (client, message, args, ops) => {
     message.channel.send(":ping_pong: Ping").then((m) => m.edit(`Pong : ${Date.now() - debut} ms:ping_pong: `));
 
     }catch(err) {
-        const errorlogs = client.channels.cache.get('714555906435711006')
+        //Dans ID CHANNEL, mettez l'ID du salon dans le quelle vous voulez que les erreurs soient envoyé
+        const errorlogs = client.channels.cache.get('ID CHANNEL)
         message.channel.send(`Ouppss, Une erreur est survenue! L'erreur à été signalée aux développeurs!`).then(msg => {msg.delete(5000)})
     
         const errembed = new Discord.MessageEmbed()
-        .setTitle("<:warn:714552336147546233> __ERREUR__")
+        .setTitle(":pushpin: __ERREUR__")
         .addField("Commande :", "`ping`")
         .addField("Erreur : ", `${err}`)
         .addField("Du serveur :", `${message.guild.name}`)
